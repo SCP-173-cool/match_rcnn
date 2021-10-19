d82e0030becff422c1c4e137349e8bb2214d38f4
vadimbartoshik <v256543130@gmail.com>
2021-10-19 16:21:12 +0300

    change fashion config

--- inference_images.py
+++ inference_images.py
@@ -33,6 +33,7 @@ class FashionConfig(Config):
     IMAGE_MIN_DIM = IMAGE_SIZE
     IMAGE_MAX_DIM = IMAGE_SIZE
     IMAGE_RESIZE_MODE = 'none'
+    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
 
 
 config = FashionConfig()
@@ -60,11 +61,13 @@ label_names = ['shirt, blouse', 'top, t-shirt, sweatshirt', 'sweater', 'cardigan
 model = modellib.MaskRCNN(mode="inference", config=config, model_dir=model_dir)
 model.load_weights(model_path, by_name=True)
 
+
 def resize_image(image_path):
     img = cv2.imread(image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_AREA)
     return img
+
 
 def refine_masks(masks, rois):
     areas = np.sum(masks.reshape(-1, masks.shape[-1]), axis=0)
@@ -74,7 +77,7 @@ def refine_masks(masks, rois):
         masks[:, :, m] = np.logical_and(masks[:, :, m], np.logical_not(union_mask))
         union_mask = np.logical_or(masks[:, :, m], union_mask)
     for m in range(masks.shape[-1]):
-        mask_pos = np.where(masks[:, :, m]==True)
+        mask_pos = np.where(masks[:, :, m] == True)
         if np.any(mask_pos):
             y1, x1 = np.min(mask_pos, axis=1)
             y2, x2 = np.max(mask_pos, axis=1)
@@ -88,16 +91,13 @@ for filename in os.listdir(input_image_dir):
         img = io.imread(image_path)
         # img = cv2.imread(image_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
         result = model.detect([resize_image(image_path)])
         r = result[0]
-
         if r['masks'].size > 0:
             masks = np.zeros((img.shape[0], img.shape[1], r['masks'].shape[-1]), dtype=np.uint8)
             for m in range(r['masks'].shape[-1]):
                 masks[:, :, m] = cv2.resize(r['masks'][:, :, m].astype('uint8'),
                                             (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
-
             y_scale = img.shape[0] / IMAGE_SIZE
             x_scale = img.shape[1] / IMAGE_SIZE
             rois = (r['rois'] * [y_scale, x_scale, y_scale, x_scale]).astype(int)
@@ -106,8 +106,9 @@ for filename in os.listdir(input_image_dir):
         else:
             masks, rois = r['masks'], r['rois']
 
-        save_masked_instances(output_image_dir, img, rois, masks, r['class_ids'], ['bg'] + label_names,  r['scores'], title='image_id',
-                               figsize=(16, 16))
+        save_masked_instances(output_image_dir, img, rois, masks, r['class_ids'], ['bg'] + label_names, r['scores'],
+                              title='image_id',
+                              figsize=(16, 16))
 
-        #display_instances_main(img, rois, masks, r['class_ids'], ['bg'] + label_names, r['scores'], title='image_id',
-        #                       figsize=(16, 16))
+        display_instances_main(img, rois, masks, r['class_ids'], ['bg'] + label_names, r['scores'], title='image_id',
+                               figsize=(16, 16))
